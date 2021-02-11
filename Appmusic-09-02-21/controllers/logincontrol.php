13faace1cd22c249dd0972d8f0542eca75af10cb
ACristeaLdv <71933993+ACristeaLdv@users.noreply.github.com>
2021-02-11 09:58:14 +0100

    Add files via upload

---
+++ Appmusic-09-02-21/controllers/logincontrol.php
@@ -0,0 +1,14 @@
+<?php 
+
+ #Lucas Fadavi Solanilla
+
+    if (isset($_POST["username"]) && isset($_POST["passcode"])) {
+
+        require_once("../models/Login.php");
+        $validar=validarDatosFinal($_POST["username"], $_POST["passcode"]); //Comprueba las credenciales 
+        header("location: ./../views/menu.php");
+
+    } else {
+        header("location: ./../index.php");
+    }
+?>
---
+++ Appmusic-09-02-21/views/menu.php
@@ -0,0 +1,30 @@
+<?php
+
+ #Lucas Fadavi Solanilla
+
+//Esto es para chequear si el usuario sigue logeado o existe una cookie
+if (isset($_COOKIE) && isset($_COOKIE["user"]) === false) {
+  exit("No estas logeado, datos incorrectos.");
+  
+}
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web musical</title>
+	<meta charset="utf-8" />
+	<meta name="author" value="Lucas Fadavi" />
+</head>
+<body>
+	<h1>Bienvenido <?php echo htmlspecialchars($_COOKIE["user"]) ?></h1>
+	<li><a href="./../controllers/logout.php">Cerrar sesión</a></li>
+	<li><a href="downmusic.php">Descargar musica</a></li>
+	<li><a href="histfacturas.php">Consultar historial de facturas</a></li>
+	<li><a href="facturas.php">Consultar facturas entre dos fechas</a></li>
+	<li><a href="ranking.php">Consultar descargas entre dos fechas</a></li>
+</body>
+</html>
+
